510864b11517261f0578e56a078b885883e1930f
blackranja <josphatranja243@gmail.com>
2023-09-22 23:24:31 +0300

    Application Setup

--- App.js
+++ App.js
@@ -13,7 +13,22 @@ const theme={
 }
 const App=()=>{
     const [loaded]=useFonts({
-        
+            InterBold:require(
+            "./assets/fonts/Inter-Bold.ttf"
+            ),
+            InterSemiBold:require(
+                './assets/fonts/Inter-SemiBold.ttf'),
+            InterMedium:require(
+                "./assets/fonts/Inter-Medium.ttf"
+            ),
+            InterRegular:require(
+                "./assets/fonts/Inter-Regular.ttf"
+            ),
+            InterBold:require(
+                "./assets/fonts/Inter-Bold.ttf"
+            ),
+
+
     })
     return(
         <NavigationContatiner theme={theme}>
